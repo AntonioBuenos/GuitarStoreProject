11e472eee34048a25697f322c55adc2b9ce5d399
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-09-29 17:14:38 +0300

    GuitarManufacturerDTO added.

--- common/src/main/java/by/smirnov/guitarstoreproject/dto/GuitarManufacturerDTO.java
+++ common/src/main/java/by/smirnov/guitarstoreproject/dto/GuitarManufacturerDTO.java
@@ -1,4 +1,20 @@
 package by.smirnov.guitarstoreproject.dto;
 
+import com.neovisionaries.i18n.CountryCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class GuitarManufacturerDTO {
+    private long id;
+    private String brand;
+    private String company;
+    private CountryCode originCountry;
+    private Timestamp creationDate;
+    private Timestamp modificationDate;
+    private boolean isDeleted;
+    private Timestamp terminationDate;
 }
