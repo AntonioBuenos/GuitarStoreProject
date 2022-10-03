b37f6fd772266d8263f08a85f485c5b8bc9e46d6
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-04 00:54:43 +0300

    Relations with DB bugs got fixed.

--- common/src/main/java/by/smirnov/guitarstoreproject/dto/InstockDTO.java
+++ common/src/main/java/by/smirnov/guitarstoreproject/dto/InstockDTO.java
@@ -2,9 +2,13 @@ package by.smirnov.guitarstoreproject.dto;
 
 import by.smirnov.guitarstoreproject.model.enums.GoodStatus;
 import by.smirnov.guitarstoreproject.model.enums.Placement;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 public class InstockDTO implements ObjectDTO{
     private Long id;
     private Long goodId;
--- common/src/main/java/by/smirnov/guitarstoreproject/dto/OrderDTO.java
+++ common/src/main/java/by/smirnov/guitarstoreproject/dto/OrderDTO.java
@@ -1,16 +1,18 @@
 package by.smirnov.guitarstoreproject.dto;
 
 import by.smirnov.guitarstoreproject.model.enums.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 public class OrderDTO implements ObjectDTO{
     private Long id;
     private String userId;
     private String instockId;
     private String deliveryAddress;
     private Timestamp creationDate;
-    private Timestamp modificationDate;
-    private Timestamp terminationDate;
     private OrderStatus orderStatus;
 }
