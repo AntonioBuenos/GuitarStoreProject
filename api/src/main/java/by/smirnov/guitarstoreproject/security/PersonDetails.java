9ec45660d0372545b1f660d36c037baae0d8763a
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-08 21:14:43 +0300

    PersonDetails created.

---
+++ api/src/main/java/by/smirnov/guitarstoreproject/security/PersonDetails.java
@@ -0,0 +1,57 @@
+package by.smirnov.guitarstoreproject.security;
+
+import by.smirnov.guitarstoreproject.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+@RequiredArgsConstructor
+public class PersonDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+}
+
