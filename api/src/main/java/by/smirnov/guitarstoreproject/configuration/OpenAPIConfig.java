7bab500bc0e61d53474d3de67abbe9f48fc0074d
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-11 09:35:52 +0300

    User validation added.

---
+++ api/src/main/java/by/smirnov/guitarstoreproject/configuration/OpenAPIConfig.java
@@ -0,0 +1,39 @@
+package by.smirnov.guitarstoreproject.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Guitar Web Store API")
+                .description("API for working with something")
+                .version("1.0")
+                .contact(apiContact())
+                .license(apiLicence());
+    }
+
+    private License apiLicence() {
+        return new License()
+                .name("MIT Licence")
+                .url("https://opensource.org/licenses/mit-license.php");
+    }
+
+    private Contact apiContact() {
+        return new Contact()
+                .name("Anton Smirnov")
+                .email("watashinodenwadesu@gmail.com")
+                .url("https://github.com/DenisBurak/FinalProject");
+    }
+}
--- common/src/main/java/by/smirnov/guitarstoreproject/model/User.java
+++ common/src/main/java/by/smirnov/guitarstoreproject/model/User.java
@@ -1,11 +1,11 @@
 package by.smirnov.guitarstoreproject.model;
 
 import by.smirnov.guitarstoreproject.model.enums.Role;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
-import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,25 +15,29 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@EqualsAndHashCode
 @Entity
 @Table(name = "users")
 public class User implements ObjectEntity{
 
-    //необходимо будет вставить валидаторы
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(max=30, message = "Address shall be up to 30 characters")
     private String address;
 
     @Column(name = "passport_number")
+    @Size(min=7, max=10, message = "Passport number length shall match 7-10 characters")
     private String passportNumber;
 
     @Column(name = "first_name")
+    @NotBlank(message = "First name cannot be empty or blank")
+    @Size(min=2, max=20, message = "First name length shall match 2-20 characters")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank(message = "Last name cannot be empty or blank")
+    @Size(min=2, max=20, message = "Last name length shall match 2-20 characters")
     private String lastName;
 
     @Enumerated(EnumType.STRING)
@@ -51,8 +55,12 @@ public class User implements ObjectEntity{
     @Column(name = "termination_date")
     private Timestamp terminationDate;
 
+    @NotBlank(message = "Login cannot be empty or blank")
+    @Size(min=2, max=30, message = "Login length shall match 2-30 characters")
     private String login;
 
+    @NotBlank(message = "Password cannot be empty or blank")
+    @Size(min=8, max=20, message = "Password length shall match 8-20 characters")
     private String password;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
