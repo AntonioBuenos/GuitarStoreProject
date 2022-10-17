0648be5dd7cf800f07221746a1928157fd215c69
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-17 08:16:18 +0300

    SpringBootStarter normalized.

--- api/src/main/java/by/smirnov/guitarstoreproject/SpringBootStarter.java
+++ api/src/main/java/by/smirnov/guitarstoreproject/SpringBootStarter.java
@@ -1,11 +1,9 @@
 package by.smirnov.guitarstoreproject;
 
+import by.smirnov.guitarstoreproject.configuration.OpenAPIConfig;
 import by.smirnov.guitarstoreproject.configuration.PersistenceProvidersConfiguration;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,17 +13,9 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "by.smirnov.guitarstoreproject")
-//@EnableWebMvc
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@Import({PersistenceProvidersConfiguration.class,
-/*        WebConfig.class,
-        SwaggerConfig.class*/
-})
+@Import({PersistenceProvidersConfiguration.class, OpenAPIConfig.class})
 @EnableCaching
-/*@OpenAPIDefinition(
-        info = @Info(title = "Web Store API", version = "1.0.0"),
-        servers = {@Server(url = "http://localhost:8081"), @Server(url = "http://smth.com")}
-)*/
 @SecurityScheme(name = "JWT Bearer",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
@@ -41,14 +31,5 @@ public class SpringBootStarter {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
-/*    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }*/
 }
 
