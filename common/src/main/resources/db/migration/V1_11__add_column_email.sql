0ed6ef0ca74884257f8096348af82daa679f65eb
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-24 11:26:27 +0300

    User entity, DTOs & converter amended (user email added).

--- common/src/main/resources/db/migration/V1_11__add_column_email.sql
+++ common/src/main/resources/db/migration/V1_11__add_column_email.sql
@@ -1,4 +1,4 @@
-alter table guitarshop.users add column email varchar(30);
+alter table guitarshop.users add column if not exists email varchar(30);
 alter table guitarshop.users alter column email set not null;
-create unique index users_email_uindex
+create unique index if not exists users_email_uindex
     on guitarshop.users (email);
