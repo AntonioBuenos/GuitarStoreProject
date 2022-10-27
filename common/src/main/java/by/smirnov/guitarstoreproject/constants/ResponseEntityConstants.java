28f10cdd2dee9e1f57ee0347b5f9d3fa2b59bae4
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-10-27 17:13:59 +0300

    ResponseEntityConstants optimized.

--- common/src/main/java/by/smirnov/guitarstoreproject/constants/ResponseEntityConstants.java
+++ common/src/main/java/by/smirnov/guitarstoreproject/constants/ResponseEntityConstants.java
@@ -5,40 +5,6 @@ import java.util.Map;
 
 public interface ResponseEntityConstants {
     String ERROR_KEY = "Error Message";
-    Map<String, String> BAD_STATUS_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Entity cannot be changed due to its status");
-    Map<String, String> BAD_GENRE_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "At least one of the genres by these IDs does not exist or is deleted");
-    Map<String, String> BAD_BRAND_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Guitar manufacturer by this ID does not exist or is deleted");
-    Map<String, String> BAD_GUITAR_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "There is no guitar in price-list by this ID");
-    Map<String, String> BAD_CUSTOMER_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Customer account by this ID does not exist or is deleted");
-    Map<String, String> BAD_INSTOCK_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "There is no available for order instock good by this ID");
-    Map<String, String> NOT_VERIFIED_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "You did non pass email verification or Your email verification code is wrong");
-    Map<String, String> ALREADY_DELETED_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Object by this ID is already deleted");
-    Map<String, String> FORBIDDEN_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Authorization check failed");
-    Map<String, String> NOT_FOUND_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Wrong ID, object not found");
-    Map<String, String> BAD_LOGIN_MAP = Collections.singletonMap(
-            ERROR_KEY,
-            "Incorrect credentials!");
-
     String BAD_STATUS_MESSAGE = "Entity cannot be changed due to its status";
     String BAD_GENRE_MESSAGE = "At least one of the genres by these IDs does not exist or is deleted";
     String BAD_BRAND_MESSAGE = "Guitar manufacturer by this ID does not exist or is deleted";
@@ -50,7 +16,6 @@ public interface ResponseEntityConstants {
     String FORBIDDEN_MESSAGE = "Authorization check failed";
     String NOT_FOUND_MESSAGE = "Wrong ID, object not found";
     String BAD_LOGIN_MESSAGE = "Incorrect credentials!";
-
     String USER_NOT_FOUND = "User not found!";
     String ORDER_STATUS = "Order Status";
     String GOOD_STATUS = "Good Status";
