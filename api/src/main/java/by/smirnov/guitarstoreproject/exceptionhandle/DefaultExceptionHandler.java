31ab138f17ca37b8c5a435c0d0e797a6b9da674d
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-05 01:42:05 +0300

    Add logs to DefaultExceptionHandler

--- api/src/main/java/by/smirnov/guitarstoreproject/exceptionhandle/DefaultExceptionHandler.java
+++ api/src/main/java/by/smirnov/guitarstoreproject/exceptionhandle/DefaultExceptionHandler.java
@@ -2,6 +2,7 @@ package by.smirnov.guitarstoreproject.exceptionhandle;
 
 import by.smirnov.guitarstoreproject.exception.NoSuchEntityException;
 import by.smirnov.guitarstoreproject.util.UUIDGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +16,12 @@ import java.util.Collections;
 import static by.smirnov.guitarstoreproject.constants.ResponseEntityConstants.ERROR_KEY;
 
 @ControllerAdvice
+@Slf4j
 public class DefaultExceptionHandler {
 
     @ExceptionHandler({NoSuchEntityException.class, EmptyResultDataAccessException.class})
     public ResponseEntity<Object> handleEntityNotFountException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.NOT_FOUND);
@@ -26,6 +29,7 @@ public class DefaultExceptionHandler {
 
     @ExceptionHandler({BadRequestException.class})
     public ResponseEntity<Object> handleBadRequestException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.BAD_REQUEST);
@@ -33,6 +37,7 @@ public class DefaultExceptionHandler {
 
     @ExceptionHandler({NotModifiedException.class})
     public ResponseEntity<Object> handleNotModifiedException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.NOT_MODIFIED);
@@ -40,6 +45,7 @@ public class DefaultExceptionHandler {
 
     @ExceptionHandler({AccessForbiddenException.class})
     public ResponseEntity<Object> handleAccessForbiddenException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.FORBIDDEN);
@@ -47,6 +53,7 @@ public class DefaultExceptionHandler {
 
     @ExceptionHandler({BadCredentialsException.class})
     public ResponseEntity<Object> handleBadCredentialsException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.BAD_REQUEST);
@@ -54,6 +61,7 @@ public class DefaultExceptionHandler {
 
     @ExceptionHandler({NumberFormatException.class})
     public ResponseEntity<Object> handleWrongNumberFormatException(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(Collections.singletonMap(
                 ERROR_KEY, getErrorContainer(e)),
                 HttpStatus.NOT_FOUND);
