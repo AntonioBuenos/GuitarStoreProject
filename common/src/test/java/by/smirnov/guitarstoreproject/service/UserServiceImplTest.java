56bc058d0aef5752028f173ddf2c4ecfb09b6bbf
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-19 12:09:13 +0300

    Unit tests for UserService ammended

--- common/src/test/java/by/smirnov/guitarstoreproject/service/UserServiceImplTest.java
+++ common/src/test/java/by/smirnov/guitarstoreproject/service/UserServiceImplTest.java
@@ -4,13 +4,26 @@ import by.smirnov.guitarstoreproject.domain.User;
 import by.smirnov.guitarstoreproject.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,6 +33,21 @@ class UserServiceImplTest {
 
     private static final long ID = 1L;
     private static final String LOGIN = "login";
+    private static final User testUser = User.builder()
+            .id(ID)
+            .address("City")
+            .creationDate(Timestamp.valueOf(LocalDateTime.of(LocalDate.of(2023,1,1), LocalTime.MIN)))
+            .email("test@test.by")
+            .firstName("test")
+            .lastName("test")
+            .login("test")
+            .password("test")
+            .isEnabled(true)
+            .isDeleted(false)
+            .build();
+
+    @Captor
+    ArgumentCaptor<User> userCaptor;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -49,6 +77,16 @@ class UserServiceImplTest {
 
     @Test
     void findAll() {
+        final Pageable pageable = mock(Pageable.class);
+        List<User> users = new ArrayList<>();
+        users.add(mock(User.class));
+        users.add(mock(User.class));
+        Page<User> page = new PageImpl<>(users);
+        doReturn(page).when(repository).findByIsDeleted(pageable, false);
+
+        List<User> actual = userService.findAll(pageable);
+
+        assertThat(users).isEqualTo(actual);
     }
 
     @Test
@@ -63,6 +101,12 @@ class UserServiceImplTest {
 
     @Test
     void delete() {
+        doReturn(Optional.of(testUser)).when(repository).findById(testUser.getId());
+        userService.delete(testUser.getId());
+
+        verify(repository).save(userCaptor.capture());
+        User value = userCaptor.getValue();
+        assertTrue(value.getIsDeleted());
     }
 
     @Test
@@ -74,5 +118,15 @@ class UserServiceImplTest {
 
     @Test
     void showDeletedUsers() {
+        final Pageable pageable = mock(Pageable.class);
+        List<User> users = new ArrayList<>();
+        users.add(mock(User.class));
+        users.add(mock(User.class));
+        Page<User> page = new PageImpl<>(users);
+        doReturn(page).when(repository).findByIsDeleted(pageable, true);
+
+        Page<User> actual = userService.showDeletedUsers(pageable);
+
+        assertThat(users).isEqualTo(actual.getContent());
     }
 }
