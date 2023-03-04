5053929807f690ddab4300100d223d5b446684f5
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-04 13:30:16 +0300

    Instocks test builder added

---
+++ common/src/test/java/by/smirnov/guitarstoreproject/builder/Instocks.java
@@ -0,0 +1,22 @@
+package by.smirnov.guitarstoreproject.builder;
+
+import by.smirnov.guitarstoreproject.domain.Instock;
+import by.smirnov.guitarstoreproject.domain.enums.GoodStatus;
+import by.smirnov.guitarstoreproject.domain.enums.Placement;
+
+import static by.smirnov.guitarstoreproject.builder.Guitars.aGuitar;
+import static by.smirnov.guitarstoreproject.constants.TestConstants.TEST_DATE_TIME;
+import static by.smirnov.guitarstoreproject.constants.TestConstants.TEST_ID;
+
+public class Instocks {
+
+    public static Instock.InstockBuilder anInstock(){
+        return Instock.builder()
+                .id(TEST_ID)
+                .guitarPosition(aGuitar().build())
+                .goodStatus(GoodStatus.AVAILABLE)
+                .placement(Placement.STORE)
+                .order(null)
+                .creationDate(TEST_DATE_TIME);
+    }
+}
