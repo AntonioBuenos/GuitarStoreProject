aa28b300ff3bbbdb9a36b92469099139b1c831cd
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-04 13:31:44 +0300

    OrderServiceImplTest optimized

--- common/src/test/java/by/smirnov/guitarstoreproject/service/OrderServiceImplTest.java
+++ common/src/test/java/by/smirnov/guitarstoreproject/service/OrderServiceImplTest.java
@@ -2,10 +2,9 @@ package by.smirnov.guitarstoreproject.service;
 
 import by.smirnov.guitarstoreproject.domain.Instock;
 import by.smirnov.guitarstoreproject.domain.Order;
-import by.smirnov.guitarstoreproject.domain.User;
 import by.smirnov.guitarstoreproject.domain.enums.GoodStatus;
-import by.smirnov.guitarstoreproject.domain.enums.OrderStatus;
 import by.smirnov.guitarstoreproject.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +21,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static by.smirnov.guitarstoreproject.builder.Instocks.anInstock;
+import static by.smirnov.guitarstoreproject.builder.Orders.anOrder;
+import static by.smirnov.guitarstoreproject.constants.TestConstants.TEST_ID;
+import static by.smirnov.guitarstoreproject.domain.enums.OrderStatus.CANCELLED;
+import static by.smirnov.guitarstoreproject.domain.enums.OrderStatus.COMPLETED;
+import static by.smirnov.guitarstoreproject.domain.enums.OrderStatus.CREATED;
+import static by.smirnov.guitarstoreproject.domain.enums.OrderStatus.SUSPENDED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -31,36 +37,35 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class OrderServiceImplTest {
 
-    private static final long ID = 1L;
-
     @InjectMocks
     private OrderServiceImpl orderService;
 
     @Mock
     private OrderRepository repository;
 
-    @Mock InstockServiceImpl instockService;
+    @Mock
+    InstockServiceImpl instockService;
 
-    final Instock testInstock = mock(Instock.class);
+    private Instock testInstock;
 
-    final Order testOrder = Order.builder()
-            .id(ID)
-            .customer(mock(User.class))
-            .deliveryAddress("test")
-            .instock(testInstock)
-            .orderStatus(OrderStatus.CREATED)
-            .build();
+    private Order testOrder;
 
     @Captor
     ArgumentCaptor<Order> orderCaptor;
+
+    @BeforeEach
+    void init() {
+        testInstock = anInstock().build();
+        testOrder = anOrder().instock(testInstock).build();
+    }
 
     @Test
     @DisplayName("findById should return Order")
     void checkFindByIdShouldReturnOrder() {
         final Order order = mock(Order.class);
-        when(repository.findById(ID)).thenReturn(Optional.ofNullable(order));
+        when(repository.findById(TEST_ID)).thenReturn(Optional.ofNullable(order));
 
-        final Order actual = orderService.findById(ID);
+        final Order actual = orderService.findById(TEST_ID);
 
         assertThat(actual).isEqualTo(order);
     }
@@ -107,11 +112,12 @@ class OrderServiceImplTest {
     void checkCompleteOrderShouldPassArgumentCompletedOrder() {
         long id = testOrder.getId();
         doReturn(Optional.of(testOrder)).when(repository).findById(id);
+
         orderService.completeOrder(id);
 
         verify(repository).save(orderCaptor.capture());
         Order value = orderCaptor.getValue();
-        assertThat(value.getOrderStatus()).isEqualTo(OrderStatus.COMPLETED);
+        assertThat(value.getOrderStatus()).isEqualTo(COMPLETED);
     }
 
     @Test
@@ -120,11 +126,12 @@ class OrderServiceImplTest {
         long id = testOrder.getId();
         doReturn(Optional.of(testOrder)).when(repository).findById(id);
         doReturn(testInstock).when(instockService).update(testInstock, GoodStatus.AVAILABLE);
+
         orderService.cancelOrder(id);
 
         verify(repository).save(orderCaptor.capture());
         Order value = orderCaptor.getValue();
-        assertThat(value.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
+        assertThat(value.getOrderStatus()).isEqualTo(CANCELLED);
     }
 
     @Test
@@ -132,31 +139,33 @@ class OrderServiceImplTest {
     void checkSuspendOrderShouldPassArgumentSuspendedOrder() {
         long id = testOrder.getId();
         doReturn(Optional.of(testOrder)).when(repository).findById(id);
+
         orderService.suspendOrder(id);
 
         verify(repository).save(orderCaptor.capture());
         Order value = orderCaptor.getValue();
-        assertThat(value.getOrderStatus()).isEqualTo(OrderStatus.SUSPENDED);
+        assertThat(value.getOrderStatus()).isEqualTo(SUSPENDED);
     }
 
     @Test
     @DisplayName("resumeOrder should pass argument CREATED")
     void checkResumeOrderShouldPassArgumentCreatedOrder() {
         long id = testOrder.getId();
-        testOrder.setOrderStatus(OrderStatus.SUSPENDED);
+        testOrder.setOrderStatus(SUSPENDED);
         doReturn(Optional.of(testOrder)).when(repository).findById(id);
+
         orderService.resumeOrder(id);
 
         verify(repository).save(orderCaptor.capture());
         Order value = orderCaptor.getValue();
-        assertThat(value.getOrderStatus()).isEqualTo(OrderStatus.CREATED);
+        assertThat(value.getOrderStatus()).isEqualTo(CREATED);
     }
 
     @Test
     @DisplayName("hardDelete should call repository")
     void checkHardDeleteShouldCallRepository() {
-        orderService.hardDelete(ID);
+        orderService.hardDelete(TEST_ID);
 
-        verify(repository).deleteById(ID);
+        verify(repository).deleteById(TEST_ID);
     }
 }
